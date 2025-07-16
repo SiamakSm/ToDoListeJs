fead574dc26c5831c072a010cd4e861b472ee4de
SiamakSm <siamak036@gmail.com>
2025-07-16 19:59:26 +0200

    Base structure

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("button").addEventListener("click", function () {
+    let nLi = document.getElementById("input").value.trim();
+    if (nLi == "") {
+        alert("New task not found!");
+    } else {
+
+    };
+});
