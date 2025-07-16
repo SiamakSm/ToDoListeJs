a414b1feb5885749eacf782ee463dca30c5d5f08
SiamakSm <siamak036@gmail.com>
2025-07-16 20:06:19 +0200

    Advanced

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.getElementById("button").addEventListener("click", function () {
-    let nLi = document.getElementById("input").value.trim();
-    if (nLi == "") {
+    let input = document.getElementById("input").value.trim();
+    if (input == "") {
         alert("New task not found!");
     } else {
-
+        let ul = document.getElementById("myListe");
+        let nLi = document.createElement("li");
+        nLi.textContent = input;
+        ul.appendChild(nLi);
+        document.getElementById("input").value = "";
     };
 });
+
